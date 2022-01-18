495d42291cc8cec62ed2eafea6c2ed9230dbf19f
Damir Kuzovatov <kuz.damir.01@gmail.com>
2022-01-18 22:41:15 +0300

    updated db url

--- models/base.py
+++ models/base.py
@@ -1,6 +1,8 @@
+import os
+
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 Base = declarative_base()
-engine = create_async_engine()
+engine = create_async_engine(os.getenv("DATABASE_URL_ASYNC"))
 create_async_session = sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession)
