492bb31349c943914ee982ae83e561d64ebb3d3e
Damir Kuzovatov <kuz.damir.01@gmail.com>
2022-01-27 19:03:20 +0300

    user update endpoint

--- api/api_v1/endpoints/users.py
+++ api/api_v1/endpoints/users.py
@@ -2,7 +2,7 @@ from typing import List, Optional
 
 from fastapi import APIRouter
 
-from schemas.users import DBUser, UserOut, UserIn
+from schemas.users import DBUser, UserOut, UserIn, DBUserUpdate
 from services.users import UsersService
 
 router = APIRouter()
@@ -10,16 +10,24 @@ router = APIRouter()
 
 @router.get("/", response_model=List[UserOut])
 async def list_users(limit: int = 100, offset: int = 0) -> List[DBUser]:
-    return await UsersService().list(limit, offset)
+    async with UsersService() as service:
+        return await service.get_all(limit, offset)
 
 
 @router.post("/")
 async def create_user(user: UserIn) -> None:
-    await UsersService().create(user)
+    user = DBUser(**user.dict())
+    async with UsersService() as service:
+        await service.create(user)
 
 
 @router.get("/{id}/", response_model=Optional[UserOut])
 async def get_user(id: int) -> Optional[DBUser]:
-    return await UsersService().retrieve_by_id(id)
+    async with UsersService() as service:
+        return await service.get_by_id(id)
 
 
+@router.patch("/{id}/")
+async def partial_update_user(id: int, user: DBUserUpdate):
+    async with UsersService() as service:
+        return await service.update(id, user)
