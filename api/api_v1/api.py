b4d976de61eb370df71e813d06fd1773d1abdf21
Damir Kuzovatov <kuz.damir.01@gmail.com>
2022-01-27 19:02:18 +0300

    mounted posts

--- api/api_v1/api.py
+++ api/api_v1/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from api.api_v1.endpoints import users
+from api.api_v1.endpoints import users, posts
 
 router = APIRouter()
 
-router.include_router(users.router, prefix="/users")
+router.include_router(users.router, prefix="/users", tags=["users"])
+router.include_router(posts.router, prefix="/posts", tags=["posts"])
