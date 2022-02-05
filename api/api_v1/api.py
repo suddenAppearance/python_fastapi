5260e6246c1f919fc66311211eaf700a419c15ab
Damir Kuzovatov <kuz.damir.01@gmail.com>
2022-02-06 01:02:31 +0300

    added file serve endpoint

--- api/api_v1/api.py
+++ api/api_v1/api.py
@@ -1,6 +1,10 @@
+import os.path
+
 from fastapi import APIRouter, Request
+from starlette.responses import FileResponse
 
 from api.api_v1.endpoints import users, posts, oauth2
+from core import get_settings
 
 router = APIRouter()
 
@@ -12,3 +16,8 @@ router.include_router(oauth2.router, prefix="/oauth2", tags=["oauth2"])
 @router.post("/")
 async def index(request: Request):
     return {"body": await request.body()}
+
+
+@router.get("/files/{image}", include_in_schema=False)  # excluding as it is not jsonschema part
+async def serve_image(image: str):
+    return FileResponse(os.path.join(get_settings().MEDIA_ROOT, image))
